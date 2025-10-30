cf7f6769041ab18b305c4255cc7701f9d1734c20
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-30 12:53:45 +0600

    added some code

---
+++ recap.js
@@ -0,0 +1,11 @@
+// const : value will not change 
+// let : value can change
+// var : we should not use it 
+
+const amount = 200;
+
+if (amount > 100) {
+    console.log("Big number");
+} else {
+    console.log("Small number");
+}
