f00026c13393634a3b917ccd3cbec46ecd3e058d
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-30 21:54:14 +0600

    added some code

--- even.js
+++ even.js
@@ -1,9 +1,41 @@
-let num = 1;
-while (num <= 10) {
-    console.log(num)
+// let num = 1;
+// while (num <= 10) {
+//     console.log(num)
 
-    if(num % 2 === 0){
-        console.log('even number ', num)
+//     if(num % 2 === 0){
+//         console.log('even number ', num)
+//     }
+//     num++;
+// }
+
+
+
+
+//  task one 
+// let day = 1;
+// while (day <= 60) {
+//     console.log(day, 'Day Number 6 house')
+//     day++;
+// }
+
+
+
+// task two 
+// let odd = 61
+// while (odd <= 100) {
+//     console.log(odd)
+//     if (odd % 2) {
+//         console.log('Odd number', odd)
+//     }
+//     odd++
+// }
+
+
+let Strengths = 80
+while (Strengths <= 98){
+    console.log(Strengths)
+    if (Strengths % 3 ){
+        console.log('Strenght number', Strengths)
     }
-    num++;
+    Strengths++
 }
