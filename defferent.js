7a97fbcabb76f5f04a5352ec239b4570bb2dbeed
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-31 20:50:11 +0600

    added some code

--- defferent.js
+++ defferent.js
@@ -1,13 +1,36 @@
 
-for (let i = 0; i < 10; i++) {
-    if (i % 2 === 1) {
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 1) {
+//         console.log(i)
+//     }
+// }
+
+
+// for (applePrice = 0; applePrice <= 500; applePrice++) {
+//     if (applePrice % 2 !== 1){
+//         console.log(applePrice)
+//     }
+// }
+
+
+
+// for (applePrice = 1; applePrice <= 500; applePrice += 2) {
+//     console.log(applePrice)
+// }
+
+
+
+// give me th elist numbers 1 to 30 divisible by 5
+
+// for (let a = 0; a <= 30; i++) {
+//     if (a % 5 === 0) {
+//         console.log(a)
+//     }
+// }
+
+
+for (let i = 1; i < 500; i++) {
+    if (i / 3 === 1  && i % 3 === 0){
         console.log(i)
-    }
-}
-
-
-for (applePrice = 0; applePrice <= 500; applePrice++) {
-    if (applePrice % 2 === 0){
-        console.log(applePrice)
     }
 }
