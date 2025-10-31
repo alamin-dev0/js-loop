da672254fd3514acbf1aec02da79f52cb9c61771
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-31 20:07:17 +0600

    added some code

---
+++ defferent.js
@@ -0,0 +1,13 @@
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
+
+
+for (applePrice = 0; applePrice <= 500; applePrice++) {
+    if (applePrice % 2 === 0){
+        console.log(applePrice)
+    }
+}
---
+++ task.js
@@ -0,0 +1,30 @@
+
+
+// Task one
+// let n = 0;
+
+// while (n <= 200) {
+//     console.log(n)
+//     n++;
+//     if (n === 100) {
+//         break;
+//     }
+//      console.log("The number is " + n )
+
+// }
+
+
+// task tow
+
+let p = 0;
+// let d = 1;
+
+
+while (p >= 100) {
+    console.log(p)
+    p++
+
+    if (p == 110) {
+        break
+    }
+}
