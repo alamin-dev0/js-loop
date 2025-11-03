eb107890ecec04bc84c20f364fcfcada587e6ee1
alamin-dev0 <alamin.dev01@gmail.com>
2025-11-03 12:21:00 +0600

    added some code

--- continue.js
+++ continue.js
@@ -1,2 +1,22 @@
 // continue -- > skip this iteration 
 // break -- > i am  done with this loop. loop end 
+
+
+// for (let i = 0; i < 100; i++) {
+//     if (i % 2 === 1) {
+//         continue
+//     }
+//     console.log(i)
+
+// }
+
+
+let a = 0
+while (a < 50) {
+    a++
+    if (a % 5 !== 0) {
+        continue
+    }
+    console.log(a)
+    
+}
